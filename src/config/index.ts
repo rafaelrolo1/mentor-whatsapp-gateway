ab479402e05402d14413a7acb46a2e24c4573f6d
rafaelrolo1 <158310549+rafaelrolo1@users.noreply.github.com>
2025-03-28 13:54:49 +0000

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 const config = {
-  secret_key: process.env.SECRET_KEY || 'default',
+  secretKey: process.env.SECRET_KEY || 'default'',
   port: process.env.PORT || 3000,
   // podes adicionar mais configs aqui depois se precisares
 };
